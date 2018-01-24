ff396c1b51910e87c8b7296603dff917b7b24428
alexkopytov <alex@top-agt.com>
2018-01-24 11:56:41 +0200

    login_without_reset

--- resources/views/frontend/auth/passwords/email.blade.php
+++ resources/views/frontend/auth/passwords/email.blade.php
@@ -1,51 +1,62 @@
 @extends('frontend.layouts.app')
 
 @section('title', app_name() . ' | Reset Password')
-
+@push('before-styles')
+    {{ style(mix('css/backend.css')) }}
+@endpush
 @section('content')
-    <div class="row justify-content-center align-items-center">
-        <div class="col col-sm-6 align-self-center">
-            <div class="card">
-                <div class="card-header">
-                    <strong>
-                        {{ __('labels.frontend.passwords.reset_password_box_title') }}
-                    </strong>
-                </div><!--card-header-->
+    @include('frontend.template.header-transparent')
 
-                <div class="card-body">
+    <section id="index-page">
+        <div class="main-top-container bg-cont">
+            <!--<div class="dark-mask"></div>-->
+            <div class="container">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col col-sm-6 align-self-center">
+                        <div class="card">
+                            <div class="card-header">
+                                <strong>
+                                    {{ __('labels.frontend.passwords.reset_password_box_title') }}
+                                </strong>
+                            </div><!--card-header-->
 
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                            <div class="card-body">
 
-                    {{ html()->form('POST', route('frontend.auth.password.email.post'))->open() }}
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
+                                @if (session('status'))
+                                    <div class="alert alert-success">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
 
-                                    {{ html()->email('email')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required()
-                                        ->autofocus() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                                {{ html()->form('POST', route('frontend.auth.password.email.post'))->open() }}
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
 
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group mb-0 clearfix">
-                                    {{ form_submit(__('labels.frontend.passwords.send_password_reset_link_button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-                    {{ html()->form()->close() }}
-                </div><!-- card-body -->
-            </div><!-- card -->
-        </div><!-- col-6 -->
-    </div><!-- row -->
+                                            {{ html()->email('email')
+                                                ->class('form-control')
+                                                ->placeholder(__('validation.attributes.frontend.email'))
+                                                ->attribute('maxlength', 191)
+                                                ->required()
+                                                ->autofocus() }}
+                                        </div><!--form-group-->
+                                    </div><!--col-->
+                                </div><!--row-->
+
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group mb-0 clearfix">
+                                            {{ form_submit(__('labels.frontend.passwords.send_password_reset_link_button')) }}
+                                        </div><!--form-group-->
+                                    </div><!--col-->
+                                </div><!--row-->
+                                {{ html()->form()->close() }}
+                            </div><!-- card-body -->
+                        </div><!-- card -->
+                    </div><!-- col-6 -->
+                </div><!-- row -->
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/frontend/auth/passwords/reset.blade.php
+++ resources/views/frontend/auth/passwords/reset.blade.php
@@ -1,78 +1,89 @@
 @extends('frontend.layouts.app')
 
 @section('title', app_name() . ' | Reset Password')
-
+@push('before-styles')
+    {{ style(mix('css/backend.css')) }}
+@endpush
 @section('content')
-    <div class="row justify-content-center align-items-center">
-        <div class="col col-sm-6 align-self-center">
-            <div class="card">
-                <div class="card-header">
-                    <strong>
-                        {{ __('labels.frontend.passwords.reset_password_box_title') }}
-                    </strong>
-                </div><!--card-header-->
+    @include('frontend.template.header-transparent')
 
-                <div class="card-body">
+    <section id="index-page">
+        <div class="main-top-container bg-cont">
+            <!--<div class="dark-mask"></div>-->
+            <div class="container">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col col-sm-6 align-self-center">
+                        <div class="card">
+                            <div class="card-header">
+                                <strong>
+                                    {{ __('labels.frontend.passwords.reset_password_box_title') }}
+                                </strong>
+                            </div><!--card-header-->
 
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                            <div class="card-body">
 
-                    {{ html()->form('POST', route('frontend.auth.password.reset'))->class('form-horizontal')->open() }}
-                        {{ html()->hidden('token', $token) }}
+                                @if (session('status'))
+                                    <div class="alert alert-success">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
 
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
+                                {{ html()->form('POST', route('frontend.auth.password.reset'))->class('form-horizontal')->open() }}
+                                {{ html()->hidden('token', $token) }}
 
-                                    {{ html()->email('email')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
 
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.password'))->for('password') }}
+                                            {{ html()->email('email')
+                                                ->class('form-control')
+                                                ->placeholder(__('validation.attributes.frontend.email'))
+                                                ->attribute('maxlength', 191)
+                                                ->required() }}
+                                        </div><!--form-group-->
+                                    </div><!--col-->
+                                </div><!--row-->
 
-                                    {{ html()->password('password')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.password'))
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            {{ html()->label(__('validation.attributes.frontend.password'))->for('password') }}
 
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.password_confirmation'))->for('password_confirmation') }}
+                                            {{ html()->password('password')
+                                                ->class('form-control')
+                                                ->placeholder(__('validation.attributes.frontend.password'))
+                                                ->required() }}
+                                        </div><!--form-group-->
+                                    </div><!--col-->
+                                </div><!--row-->
 
-                                    {{ html()->password('password_confirmation')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.password_confirmation'))
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            {{ html()->label(__('validation.attributes.frontend.password_confirmation'))->for('password_confirmation') }}
 
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group mb-0 clearfix">
-                                    {{ form_submit(__('labels.frontend.passwords.reset_password_button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-                    {{ html()->form()->close() }}
-                </div><!-- card-body -->
-            </div><!-- card -->
-        </div><!-- col-6 -->
-    </div><!-- row -->
+                                            {{ html()->password('password_confirmation')
+                                                ->class('form-control')
+                                                ->placeholder(__('validation.attributes.frontend.password_confirmation'))
+                                                ->required() }}
+                                        </div><!--form-group-->
+                                    </div><!--col-->
+                                </div><!--row-->
+
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group mb-0 clearfix">
+                                            {{ form_submit(__('labels.frontend.passwords.reset_password_button')) }}
+                                        </div><!--form-group-->
+                                    </div><!--col-->
+                                </div><!--row-->
+                                {{ html()->form()->close() }}
+                            </div><!-- card-body -->
+                        </div><!-- card -->
+                    </div><!-- col-6 -->
+                </div><!-- row -->
+            </div>
+        </div>
+    </section>
 @endsection
