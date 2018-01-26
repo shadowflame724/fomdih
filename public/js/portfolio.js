4224940b4648a410bddf3170e01afeeb761c54a0
glalex <oleksii.hlaholiev@gmail.com>
2018-01-26 17:50:38 +0200

    update scripts

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -18,7 +18,7 @@ $(function () {
     resizeWindowHandler(); // initial call!!!
 
     // turn on this script only on pages with portfolio block (our-works)
-    if ($("#our-works").length != 0) {
+    if ($("#our-works").length != 0 && window.innerWidth >= mobileViewWidth) {
         // start of SPI jena FROM http://sticky01.blogspot.ru/2013/09/5.html
         (function(){
             var a = document.querySelector('.our-works .left-cont .portfolio-menu'), b = null, P = 0;
@@ -27,43 +27,41 @@ $(function () {
             window.addEventListener('scroll', Ascroll, false);
             document.body.addEventListener('scroll', Ascroll, false);
             function Ascroll() {
-                if (window.innerWidth >= mobileViewWidth) {
-                    if (b == null) {
-                        var Sa = getComputedStyle(a, ''), s = '';
-                        for (var i = 0; i < Sa.length; i++) {
-                            if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-                                s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-                            }
+                if (b == null) {
+                    var Sa = getComputedStyle(a, ''), s = '';
+                    for (var i = 0; i < Sa.length; i++) {
+                        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+                            s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
                         }
-                        b = document.createElement('div');
-                        b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-                        a.insertBefore(b, a.firstChild);
-                        var l = a.childNodes.length;
-                        for (var i = 1; i < l; i++) {
-                            b.appendChild(a.childNodes[1]);
-                        }
-                        a.style.height = b.getBoundingClientRect().height + 'px';
-                        a.style.padding = '0';
-                        a.style.border = '0';
                     }
-                    var Ra = a.getBoundingClientRect(),
-                        R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.our-works-cont .right-cont').getBoundingClientRect().bottom + 1.2 * morePortfolioBtn.getBoundingClientRect().height);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
-                    if ((Ra.top - P) <= 0) {
-                        if ((Ra.top - P) <= R) {
-                            b.className = 'stop';
-                            b.style.top = - R +'px';
-                        } else {
-                            b.className = 'sticky';
-                            b.style.top = P + 'px';
-                        }
-                    } else {
-                        b.className = '';
-                        b.style.top = '';
+                    b = document.createElement('div');
+                    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+                    a.insertBefore(b, a.firstChild);
+                    var l = a.childNodes.length;
+                    for (var i = 1; i < l; i++) {
+                        b.appendChild(a.childNodes[1]);
                     }
-                    window.addEventListener('resize', function() {
-                        a.children[0].style.width = getComputedStyle(a, '').width
-                    }, false);
+                    a.style.height = b.getBoundingClientRect().height + 'px';
+                    a.style.padding = '0';
+                    a.style.border = '0';
                 }
+                var Ra = a.getBoundingClientRect(),
+                    R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.our-works-cont .right-cont').getBoundingClientRect().bottom + 1.2 * morePortfolioBtn.getBoundingClientRect().height);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
+                if ((Ra.top - P) <= 0) {
+                    if ((Ra.top - P) <= R) {
+                        b.className = 'stop';
+                        b.style.top = - R +'px';
+                    } else {
+                        b.className = 'sticky';
+                        b.style.top = P + 'px';
+                    }
+                } else {
+                    b.className = '';
+                    b.style.top = '';
+                }
+                window.addEventListener('resize', function() {
+                    a.children[0].style.width = getComputedStyle(a, '').width
+                }, false);
             }
         })()
         // end of SPI jena FROM http://sticky01.blogspot.ru/2013/09/5.html
