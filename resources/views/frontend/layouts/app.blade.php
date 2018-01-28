a911bf02eb94e58f4f217cb693aea8ce6fc8ef8a
glalex <oleksii.hlaholiev@gmail.com>
2018-01-28 16:32:48 +0200

    add Google Analytics

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -31,5 +31,15 @@
 </script>
 @stack('after-scripts')
 
+{{-- Global site tag (gtag.js) - Google Analytics --}}
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113127040-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-113127040-1');
+</script>
+
 </body>
 </html>
