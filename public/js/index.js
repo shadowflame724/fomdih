7494f4283de0f7a1f3eeeb3c51c7478984eef2fa
glalex <oleksii.hlaholiev@gmail.com>
2018-02-02 23:37:05 +0200

    update minified styles and scripts

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,1 @@
-"use strict";
-
-$(function () {
-    // ANIMATION BLOCK
-    if (!isMobileViewFlag) {
-        // ****************************************************************************
-        // *************    ANIMATIONS FOR THIS PAGE    *************
-
-        $('.img-cont .block-line').addClass("invisible").viewportChecker({
-            classToAdd: 'animated fadeInDown',
-            classToRemove: 'visible',
-            offset: '10%',
-            repeat: false
-        });
-
-    }
-
-});
+"use strict";$(function(){isMobileViewFlag||$(".img-cont .block-line").addClass("invisible").viewportChecker({classToAdd:"animated fadeInDown",classToRemove:"visible",offset:"10%",repeat:!1})});
